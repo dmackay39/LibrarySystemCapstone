6f56f045f5000c642deea6b67af67d02d031739b
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-11-17 09:24:16 +0000

    First commit

---
+++ src/test/java/com/example/LibrarySystemCapstone/LibrarySystemCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibrarySystemCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
